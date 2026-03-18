2de42a5912fe6af0bb81017f89d61cd6015b6448
Colin <e1355879@u.nus.edu>
2026-03-19 00:26:53 +0800

    Update sample data to use LinkedHashSet for pet list
    Now, first two have one pet each, the rest have no pets

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,6 +1,8 @@
 package seedu.address.model.util;
 
 import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,35 +29,29 @@ public class SampleDataUtil {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
                 getTagSet("friends"),
-                Set.of(new Pet(new PetName("Buddy"), new Species("Dog"), new OwnerIndex("1"),
-                        new PetRemark("Loyal and friendly")))),
+                new LinkedHashSet<>(List.of(new Pet(new PetName("Buddy"), new Species("Dog"), new OwnerIndex("1"),
+                        new PetRemark("Loyal and friendly"))))),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                 getTagSet("colleagues", "friends"),
-                Set.of(new Pet(new PetName("Mittens"), new Species("Cat"), new OwnerIndex("2"),
-                        new PetRemark("Likes to scratch furniture")))),
+                new LinkedHashSet<>(List.of(new Pet(new PetName("Mittens"), new Species("Cat"), new OwnerIndex("2"),
+                        new PetRemark("Likes to scratch furniture"))))),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                 getTagSet("neighbours"),
-                Set.of(new Pet(new PetName("Goldy"), new Species("Fish"), new OwnerIndex("3"),
-                        new PetRemark("Is always calm")))),
+                new LinkedHashSet<>()),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                 getTagSet("family"),
-                Set.of(new Pet(new PetName("Charlie"), new Species("Bird"), new OwnerIndex("4"),
-                        new PetRemark("Sings beautifully")))),
+                new LinkedHashSet<>()),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
                 getTagSet("classmates"),
-                Set.of(new Pet(new PetName("Thumper"), new Species("Rabbit"), new OwnerIndex("5"),
-                        new PetRemark("Loves carrots")))),
+                new LinkedHashSet<>()),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
                 getTagSet("colleagues"),
-                Set.of(new Pet(new PetName("Spike"), new Species("Hedgehog"), new OwnerIndex("6"),
-                        new PetRemark("Very shy but cute"))))
-
-
+                new LinkedHashSet<>())
         };
     }
 
