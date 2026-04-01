1e06bf8ffff92007f97d1ce50f4bfbf863e72a38
Justin Lee <justinleetw@gmail.com>
2026-04-01 15:40:51 +0800

    Improve phone regex

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,9 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers should only contain numbers, \"+\", \"-\" or spaces, and it should be at least 3-15 "
+                    + "characters long";
+    public static final String VALIDATION_REGEX = "[0-9+\\- ]{2,14}[0-9]";
     public final String value;
 
     /**
