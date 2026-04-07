fce817f02c2ee4367788b39609d2e74ae85b1b2a
nana-6381 <190936945+nana-6381@users.noreply.github.com>
2026-04-07 18:11:57 +0800

    Fix checktyle violations in EditCommandParser and EditCommand class

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -177,7 +177,8 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags, pets, tagsToAdd, tagsToRemove, tagsToRemove);
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags,
+                    pets, tagsToAdd, tagsToRemove, tagsToRemove);
         }
 
         public void setName(Name name) {
