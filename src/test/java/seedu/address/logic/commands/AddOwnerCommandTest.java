748d1dbf0cbc12fb86e15e323072e2b754342332
aurorazurary <aurorazurary@gmail.com>
2026-04-09 19:28:48 +0800

    Fix checkstyle problem

--- src/test/java/seedu/address/logic/commands/AddOwnerCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddOwnerCommandTest.java
@@ -23,7 +23,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.model.pet.Pet;
 import seedu.address.model.service.Service;
 import seedu.address.model.session.SessionEntry;
 import seedu.address.testutil.PersonBuilder;
